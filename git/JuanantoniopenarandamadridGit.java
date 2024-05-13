e92f5d7422f25cf17b4e678d04606a951bd9aa8a
juanantoniopenaranda <1889175@alu.murciaeduca.es>
2024-05-14 01:20:54 +0200

    subir repositorio online

---
+++ git/JuanantoniopenarandamadridGit.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package juanantoniopenarandamadrid.git;
+
+/**
+ * CAMBIO CODIGO 
+ * 
+ * 
+ * @author JUAN
+ */
+public class JuanantoniopenarandamadridGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+/** EJEMPLO DE CONFLICTO DE RAMA
+ * 
+ */
+
+/** Cambio de vuelta a la rama master 
+>>>>>>> RAMA1
+ * 
+ */
